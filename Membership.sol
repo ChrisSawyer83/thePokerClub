031a4ca91699dc3ff64a21dedaa12ac7a69c22ac
ChrisSawyer83 <47927367+ChrisSawyer83@users.noreply.github.com>
2022-01-14 13:19:47 -0800

    Update Membership.sol

--- Membership.sol
+++ Membership.sol
@@ -4,24 +4,29 @@ pragma solidity ^0.6.0;
 
 contract Membership {
 
-    event NewMember(uint memberId, string name, uint level);
-
-     struct ClubMember {        //member information
-        uint32 memberId;
+    event newMember(uint memberId, string name, uint level);
+    
+    //struct packed for gas optimization
+     struct ClubMember {                         
         string name;
+        uint32 memberId;
         uint16 level;
         uint16 winCount;
         uint16 lossCount;
     }
 
-    ClubMember[] public members;       //dynamic array for storage of structured member data
+    //dynamic public array as database for new members
+    ClubMember[] public members;                 
 
-    function _createMember(string memory _name, uint _memberId) public {
-        members.push(ClubMember(_name, _memberId));
+    //member Id assignment mapping
+    mapping (uint => address) public memberIdOwner;      
+
+    //check to ensure addresses can only register as a member once
+    //new member appended to the end of members array
+    function createMember(string memory _name, uint _memberId, uint _level) public {
+        require(memberIdOwner[msg.sender] == 0);                            
+        uint id = members.push(ClubMember(_name, _memberId, i=1)) - 1;      
+        memberIdOwner[id] = msg.sender;
+        emit newMember(id, _name, _level);
     }
-
-    function getMemberId() public view returns (uint){
-        return memberId;
-    }
-
 }
