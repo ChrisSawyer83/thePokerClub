9b435d6f7f2aed52244297e92bd990a8e3de0d9a
ChrisSawyer83 <47927367+ChrisSawyer83@users.noreply.github.com>
2022-01-24 13:15:32 -0800

    Update Membership.sol

--- Membership.sol
+++ Membership.sol
@@ -2,7 +2,9 @@
 
 pragma solidity ^0.6.0;
 
-contract Membership {
+import "./tokenGeneration.sol/";
+
+contract clubMembership is tokenGeneration {
 
     event newMember(uint memberId, string name, uint level);
     
@@ -11,22 +13,31 @@ contract Membership {
         string name;
         uint32 memberId;
         uint16 level;
-        uint16 winCount;
-        uint16 lossCount;
     }
 
     //dynamic public array as database for new members
     ClubMember[] public members;                 
 
     //member Id assignment mapping
-    mapping (uint => address) public memberIdOwner;      
+    mapping (uint => address) public memberIdOwner;
+    mapping (address => uint) public balance;
 
-    //check to ensure addresses can only register as a member once
-    //new member appended to the end of members array
+    //new member creation and account funding
+    //check to ensure addresses can only register as a member once 
+    //new member appended to the end of members array as Level 1 member
+    //assigns memberId to function caller
+    //returns new member information to front end
     function createMember(string memory _name, uint _memberId, uint _level) public {
-        require(memberIdOwner[msg.sender] == 0);                            
-        uint id = members.push(ClubMember(_name, _memberId, i=1)) - 1;      
-        memberIdOwner[id] = msg.sender;
+        require(memberIdOwner[msg.sender] == 0);                                           
+        uint id = members.push(ClubMember(_name, _memberId, i=1)) - 1;           
+        memberIdOwner[id] = msg.sender;                                 
         emit newMember(id, _name, _level);
+    
+    //transfer 1000 TPCCToken when the new club member is added to the array
+        for(uint256 i=0; i < addressArray.length; i++) {
+        require(msg.sender = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        address payable addr = addressArray[i];
+        addr.transfer(1000 TPCCToken);
     }
+
 }
